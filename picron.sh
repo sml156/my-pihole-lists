9caaafce24478ec8ae7d300c6061fd1d184f5594
sml156 <sml156@hotmail.com>
2019-10-17 01:19:01 -0400

    <added files>

--- picron.sh
+++ picron.sh
@@ -5,10 +5,11 @@ cd /usr/local/bin/my-pihole-lists/
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=all | tee -a /etc/pihole/adlists.list 1>/dev/null
 
-#pihole -g
+
+pihole -g
 
 
-#sleep 5
+sleep 5
 
 cp /etc/pihole/adlists.list /usr/local/bin/my-pihole-lists/adlists.list
 cp /etc/pihole/black.list /usr/local/bin/my-pihole-lists/black.list
