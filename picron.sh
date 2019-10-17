81aa8423b5b260fc36e36007093a967520786cfb
sml156 <sml156@hotmail.com>
2019-10-17 17:25:15 -0400

    change settings

--- picron.sh
+++ picron.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null
+#wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=all | tee -a /etc/pihole/adlists.list 1>/dev/null
 
 #pihole -g --skip-download
-pihole -g
+#pihole -g
 
 
 sleep 5
@@ -17,13 +17,14 @@ cp /etc/pihole/dns-servers.conf /usr/local/bin/my-pihole-lists/dns-servers.conf
 cp /etc/pihole/regex.list /usr/local/bin/my-pihole-lists/regex.list
 cp /etc/pihole/whitelist.txt /usr/local/bin/my-pihole-lists/whitelist.txt
 
+cd /usr/local/bin/my-pihole-lists/
 git add *
 git commit -m "change settings"
 git push -u origin master
 git status
 
 
-cd /usr/local/bin/my-pihole-lists/
+# cd /usr/local/bin/my-pihole-lists/
 /usr/local/bin/pihole-cloudsync/pihole-cloudsync --push
 
 date > /home/pi11/datepicron1017
