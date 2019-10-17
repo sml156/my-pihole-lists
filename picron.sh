05ce0b58e0a60d487b53964f77f70be76501c8e8
sml156 <sml156@hotmail.com>
2019-10-17 17:17:20 -0400

    Updated October 17, 2019  5:17PM

--- picron.sh
+++ picron.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-cd /usr/local/bin/my-pihole-lists/
 
-#wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null
+wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=all | tee -a /etc/pihole/adlists.list 1>/dev/null
 
-
+#pihole -g --skip-download
 pihole -g
 
 
@@ -19,12 +18,12 @@ cp /etc/pihole/regex.list /usr/local/bin/my-pihole-lists/regex.list
 cp /etc/pihole/whitelist.txt /usr/local/bin/my-pihole-lists/whitelist.txt
 
 git add *
-git commit -m "<added files>"
+git commit -m "change settings"
 git push -u origin master
 git status
 
 
+cd /usr/local/bin/my-pihole-lists/
+/usr/local/bin/pihole-cloudsync/pihole-cloudsync --push
 
-#/usr/local/bin/pihole-cloudsync/pihole-cloudsync --push
-
-#date > /home/pi11/datepicron
+date > /home/pi11/datepicron1017
